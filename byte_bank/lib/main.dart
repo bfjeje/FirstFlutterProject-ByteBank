352d9ed40c100547ecb0d4a44081c256f68b4bf8
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2021-09-16 22:15:41 -0300

    Created the first root widget, and added some texts and columns

--- byte_bank/lib/main.dart
+++ byte_bank/lib/main.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/material.dart';
 
 //runApp hace que lo que pongamos dentro se ejecute en una app, y no en la terminal
-void main() => runApp(const Text("Hello World", textDirection: TextDirection.ltr));
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          const Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          const Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          const Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: const <Widget>[
+              Text(
+                'Craft beautiful UIs',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
