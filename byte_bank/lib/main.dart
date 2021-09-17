5a1d9ae4d50fc242f0b334c390d0d72399dcd585
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2021-09-16 21:58:50 -0300

    First commit

---
+++ byte_bank/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+//runApp hace que lo que pongamos dentro se ejecute en una app, y no en la terminal
+void main() => runApp(const Text("Hello World", textDirection: TextDirection.ltr));
